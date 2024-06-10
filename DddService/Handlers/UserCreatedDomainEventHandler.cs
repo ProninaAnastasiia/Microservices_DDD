0df3fe352240dffa3e19b70e34243a92e7041933
ProninaAnastasiia <soul_bler@mail.ru>
2024-06-10 17:29:24 +0300

    small fixes and ClickHouse commands added as txt file

--- DddService/Handlers/UserCreatedDomainEventHandler.cs
+++ DddService/Handlers/UserCreatedDomainEventHandler.cs
@@ -7,19 +7,19 @@ namespace DddService.Handlers;
 public class UserCreatedDomainEventHandler: INotificationHandler<UserCreatedDomainEvent>
 {
     private readonly ILogger<UserCreatedDomainEventHandler> _logger;
-    private readonly KafkaProducerService _kafkaProducerService;
     
-    public UserCreatedDomainEventHandler(KafkaProducerService kafkaProducerService, ILogger<UserCreatedDomainEventHandler> logger)
+    public UserCreatedDomainEventHandler(ILogger<UserCreatedDomainEventHandler> logger)
     {
-        _kafkaProducerService = kafkaProducerService;
         _logger = logger;
     }
 
     public Task Handle(UserCreatedDomainEvent notification, CancellationToken cancellationToken)
     {
-        _logger.LogInformation($"New user created {DateTime.Now}: ID={notification.Id}, Surename={notification.Surename}, " +
-                               $"Name={notification.Name}, PassportNumber={notification.PassportNumber}, DateOfBirth={notification.DateOfBirth}," +
-                               $"DateOfPassportExpiry={notification.DateOfPassportExpiry}, DateOfTicketExpiry={notification.DateOfTicketExpiry}, TicketPrice={notification.TicketPrice}");
+        _logger.LogInformation($"New user created {DateTime.Now}: ID={notification.Id}, " +
+                               $"Surename={notification.Surename}, Name={notification.Name}, " +
+                               $"PassportNumber={notification.PassportNumber}, DateOfBirth={notification.DateOfBirth}," +
+                               $"DateOfPassportExpiry={notification.DateOfPassportExpiry}, " +
+                               $"DateOfTicketExpiry={notification.DateOfTicketExpiry}, TicketPrice={notification.TicketPrice}");
        
         return Task.CompletedTask;
     }
